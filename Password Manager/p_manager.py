a8be7c0bc9c79901b36b6b7fa29db298e340bf78
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-19 12:30:07 +0300

    Added a class for the users' profile

--- Password Manager/p_manager.py
+++ Password Manager/p_manager.py
@@ -19,6 +19,13 @@ So in for our application we will need only:
 """
 
 
+class User:
+    def __init__(self, first_name, last_name, profile_key):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.profile_key = profile_key
+
+
 class Passwords:
     """
     Class that generates new instances of contacts.
